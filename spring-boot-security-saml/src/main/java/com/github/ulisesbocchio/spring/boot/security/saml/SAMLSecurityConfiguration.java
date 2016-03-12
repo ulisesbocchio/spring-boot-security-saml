4ede8d3b47a8e75c75a9bac5584ac19ad424777e
Ulises Bocchio <Sergio.U.Bocchio@Disney.com>
2016-03-11 18:33:18 -0800

    initial class

---
+++ spring-boot-security-saml/src/main/java/com/github/ulisesbocchio/spring/boot/security/saml/SAMLSecurityConfiguration.java
@@ -0,0 +1,11 @@
+package com.github.ulisesbocchio.spring.boot.security.saml;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Ulises Bocchio, Sergio.U.Bocchio@Disney.com (BOCCS002)
+ */
+@Configuration
+public class SAMLSecurityConfiguration {
+
+}
---
+++ spring-boot-security-saml/src/test/java/com/github/ulisesbocchio/spring/boot/security/saml/SAMLSecurityConfigurationTest.java
@@ -0,0 +1,15 @@
+package com.github.ulisesbocchio.spring.boot.security.saml;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Ulises Bocchio, Sergio.U.Bocchio@Disney.com (BOCCS002)
+ */
+public class SAMLSecurityConfigurationTest {
+    @Test
+    public void dummyTest() {
+        Assert.assertTrue(true);
+    }
+
+}
